2f35ecfbcc10baa87350e6381baa79abacb8765a
Peixotim <pedropeixotovz@gmail.com>
2025-10-27 17:10:48 -0300

    feat: Adiciona construtor para a Entidade

--- src/messages/entitty/messages.entity.ts
+++ src/messages/entitty/messages.entity.ts
@@ -4,4 +4,18 @@ export class MessagesEntity {
   description: string;
   to: string;
   status: string;
+
+  constructor(
+    id: string,
+    name: string,
+    desc: string,
+    to: string,
+    status: string,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = desc;
+    this.to = to;
+    this.status = status;
+  }
 }
