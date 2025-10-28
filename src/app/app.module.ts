4718e6e18d2ba51c5884fa8ed9635c093bb02194
Peixotim <pedropeixotovz@gmail.com>
2025-10-27 21:59:23 -0300

    Feat:Adiciona o TypeORM e mostra como configurar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      database: 'nest-db',
+      password: 'postgres',
+      autoLoadEntities: true, //Carrega entidades sem especificar-las
+      synchronize: true, //Sincroniza tudo oque fizemos aqui com o banco de dados (Nao devemos utilizar em producao)
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
