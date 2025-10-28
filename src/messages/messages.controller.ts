cfc5429ab0ace9fe4f44d61b8e5335185b47ddba
Peixotim <pedropeixotovz@gmail.com>
2025-10-28 10:43:55 -0300

    Feat: Features

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesRequest } from './dtos/messages.request.dto';
+import { MessagesEntity } from './entitty/messages.entity';
 
 @Controller('messages')
 export class MessagesController {
@@ -10,5 +18,11 @@ export class MessagesController {
   @HttpCode(HttpStatus.CREATED)
   public create(@Body() request: MessagesRequest) {
     return this.messagesService.create(request);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  public async findAll(): Promise<MessagesEntity[]> {
+    return await this.messagesService.getAll();
   }
 }
